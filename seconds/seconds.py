f4238f4c06b50c8998506ea2e4aed78438e04d86
Dóczi Szilárd <doczi.szilard@gmail.com>
2017-04-10 14:31:42 +0200

    Fuction wich returns back every second element from a list

--- seconds/seconds.py
+++ seconds/seconds.py
@@ -1,5 +1,7 @@
-def seconds(change_this):
-    pass
+def seconds(alist):
+    new_list = alist[1::2]
+    return new_list
+
     # Create a function that takes a list as a parameter,
     # and returns a new list with every second element from the orignal list
     # example: [1, 2, 3, 4, 5] should produce [2, 4]
